c5f31b13c624171de91b1d0bea588c27eb37db17
iintothewind <ivarchen@live.cn>
2026-05-31 21:10:42 -0700

    reset to normal walking speed according to user feedback

--- data/scripts/think/main.c
+++ data/scripts/think/main.c
@@ -341,7 +341,9 @@ void enemyAI()
 
 	if(walking){
 		if(aimove == openborconstant("AIMOVE1_AVOID")){
-			float rate = calcWalkSpeedRate(self);
+			// float rate = calcWalkSpeedRate(self);
+			// reset to normal walking speed according to user feedback
+			float rate = 1.0;
 			if(xDir != 0){changeentityproperty(self, "velocity", xDir*rate, NULL());}
 			if(zDir != 0){changeentityproperty(self, "velocity", NULL(), zDir*rate);}
 		}
